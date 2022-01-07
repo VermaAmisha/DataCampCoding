0a4078168c7949781221599d047fbdf159da9c2f
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-07 14:20:58 +0530

    A program using matplotlib package.

---
+++ matplotlib.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+year = [1950, 1970, 1990, 2010]
+pop = [2.517, 3.692, 5.263, 6.972]
+plt.plot(year, pop)     # to plot the data - 1st argument in x-axis 2nd argument in y-axis
+plt.show()              # to display the plot on screen
