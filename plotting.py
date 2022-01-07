6f689cb3c4c3ab0ecf81e1a7c2e1ec9fb18c843f
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-07 23:07:18 +0530

    A program to plot a line graph.

---
+++ plotting.py
@@ -0,0 +1,16 @@
+# Print the last item from year and pop
+year = [2100]
+pop = [10.85]
+
+print(year)
+print(pop)
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Make a line plot: year on the x-axis, pop on the y-axis
+
+plt.plot(year,pop) 
+
+# Display the plot with plt.show()
+plt.show()
