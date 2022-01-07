4ea7031ba25099fe6f6755438967915c76529901
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-07 23:03:43 +0530

    A program using matplotlib package to draw histogram.

---
+++ histogram.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+life_exp = [76.384, 73.747, 74.249, 73.422, 62.698, 42.38399999999999, 43.487]
+
+# Create histogram of life_exp data
+plt.hist(life_exp)
+
+# Display histogram
+plt.show()
