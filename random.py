2978ee33887b5642004d069522b735e9a9c70c4a
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-10 00:34:29 +0530

    A program using random in numpy.

---
+++ random.py
@@ -0,0 +1,10 @@
+
+# Import numpy as np
+import numpy as np
+
+# Set the seed
+np.random.seed(123)
+# seed id set so that the results are reproducable between simulations
+
+# Generate and print random float
+print(np.random.rand())
