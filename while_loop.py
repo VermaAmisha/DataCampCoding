3566bc5ce0607f9cd5c4e49ed9891677664fb9aa
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-10 00:40:05 +0530

    A program using while loop.

---
+++ while_loop.py
@@ -0,0 +1,20 @@
+# Initialize offset
+offset = 8
+
+# Code the while loop
+while offset != 0:
+    print("correcting...")
+    offset = offset - 1
+    print(offset)
+
+# Initialize offset
+offset = -6
+
+# Code the while loop
+while offset != 0 :
+    print("correcting...")
+    if offset > 0 :
+      offset = offset - 1
+    else : 
+       offset = offset + 1   
+    print(offset)
