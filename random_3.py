32a483dcec526d13c0ec62d60ac9ee66de960a5a
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-10 00:26:22 +0530

    A program using random in numpy.

---
+++ random_3.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+# Starting step
+step = 50
+
+# Roll the dice
+dice = np.random.randint(1,7)
+
+# Finish the control construct
+if dice <= 2 :
+    step = step - 1
+elif dice <=5 :
+    step = step + 1
+else :
+    step = step + np.random.randint(1,7)
+
+# Print out dice and step
+print(dice, step)
