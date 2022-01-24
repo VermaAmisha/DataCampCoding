13f6e1d65da663a18df39b292a7d3ae4be1c5578
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-24 14:02:52 +0530

    A program to convert lists into dictionary and zip them

---
+++ zip_dictionary.py
@@ -0,0 +1,11 @@
+feature_names=  ['CountryName', 'CountryCode', 'IndicatorName', 'IndicatorCode', 'Year', 'Value']
+row_vals=  ['Arab World', 'ARB', 'Adolescent fertility rate (births per 1,000 women ages 15-19)', 'SP.ADO.TFRT', '1960', '133.56090740552298']
+
+# Zip lists: zipped_lists
+zipped_lists = zip(feature_names,row_vals)
+
+# Create a dictionary: rs_dict
+rs_dict = dict(zipped_lists)
+
+# Print the dictionary
+print(rs_dict)
