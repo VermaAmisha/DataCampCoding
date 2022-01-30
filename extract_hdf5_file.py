2c9cb738d4f4a828df212c7c8a4d4fbd14330f8b
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-01-30 21:53:23 +0530

    A program to extract data from HDF5 file.

---
+++ extract_hdf5_file.py
@@ -0,0 +1,32 @@
+# Import packages
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+# Assign filename: file
+file = 'LIGO_data.hdf5'
+
+# Load file: data
+data = h5py.File(file, 'r')
+
+# Get the HDF5 group: group
+group = data['strain']
+
+# Check out keys of group
+for key in group.keys():
+    print(key)
+
+# Set variable equal to time series data: strain
+strain = data['strain']['Strain'].value
+
+# Set number of time points to sample: num_samples
+num_samples = 10000
+
+# Set time vector
+time = np.arange(0, 1, 1/num_samples)
+
+# Plot data
+plt.plot(time, strain[:num_samples])
+plt.xlabel('GPS Time (s)')
+plt.ylabel('strain')
+plt.show()
