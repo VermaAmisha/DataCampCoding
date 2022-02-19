b5eeb9a98cecd180acf2dc97ac8518bc96718589
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-02-19 23:29:29 +0530

    A program using missingno package to plot matrix of missing values.

---
+++ missingno_package.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import missingno as msno
+
+# Print number of missing values in banking
+print(banking.isna().sum())
+
+# Visualize missingness matrix
+msno.matrix(banking)
+plt.show()
+
+# Isolate missing and non missing values of inv_amount
+missing_investors = banking[banking['inv_amount'].isna()]
+investors = banking[~banking['inv_amount'].isna()]
+
+# Sort banking by age and visualize
+banking_sorted = banking.sort_values(by = 'age')
+msno.matrix(banking_sorted)
+plt.show()
