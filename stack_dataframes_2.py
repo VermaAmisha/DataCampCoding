02f283ab637095dfe3e7d42af26631d7144ff63a
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-02-24 23:56:36 +0530

    A program to stack the dataframe.

---
+++ stack_dataframes_2.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# Predefined list to use as index
+new_index = [['California', 'California', 'New York', 'Ohio'], 
+             ['Los Angeles', 'San Francisco', 'New York', 'Cleveland']]
+
+# Create a multi-level index using predefined new_index
+churn_new = pd.MultiIndex.from_arrays(new_index, names=['state', 'city'])
+
+# Assign the new index to the churn index
+churn.index = churn_new
+
+# Reshape by stacking churn DataFrame
+churn_stack = churn.stack()
+
+# Print churn_stack
+print(churn_stack)
