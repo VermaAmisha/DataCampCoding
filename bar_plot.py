bbef22bd72df9581c4dc321e57fa84a5744f1432
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-03-10 00:46:28 +0530

    A program to create boxplot to get additional information about distribution of the data.

---
+++ bar_plot.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+# Add a boxplot for the "Height" column in the DataFrames
+ax.boxplot([mens_rowing['Height'], mens_gymnastics['Height']])
+
+# Add x-axis tick labels:
+ax.set_xticklabels(["Rowing","Gymnastics"])
+
+# Add a y-axis label
+ax.set_ylabel("Height (cm)")
+
+plt.show()
