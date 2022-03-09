8b506ad90691b577f0e5104e105e069746dae427
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-03-10 00:12:16 +0530

    A program to put error bars in both the data

---
+++ error_bars.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+
+# Add a bar for the rowing "Height" column mean/std
+ax.bar("Rowing",mens_rowing['Height'].mean(), yerr=mens_rowing['Height'].std())
+
+# Add a bar for the gymnastics "Height" column mean/std
+ax.bar("Gymnastics",mens_gymnastics['Height'].mean(), yerr =mens_gymnastics['Height'].std())
+
+# Label the y-axis
+ax.set_ylabel("Height (cm)")
+
+plt.show()
