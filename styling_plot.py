f2fa8334d42c8f3c830f169a2e763fe4be2b3d17
VermaAmisha <91117662+VermaAmisha@users.noreply.github.com>
2022-03-11 21:17:07 +0530

    A program to style the plot.

---
+++ styling_plot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+# Use the "Solarize_Light2" style and create new Figure/Axes
+plt.style.use("Solarize_Light2")
+fig , ax = plt.subplots()
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"])
+plt.show()
